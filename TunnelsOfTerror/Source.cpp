b9b0bec438c5cfb4a9addbcbf9ce513068fb07a8
Drzewiecki G P (FCES) <17069130@glam.ac.uk>
2019-10-17 12:57:20 +0100

    update

--- TunnelsOfTerror/Source.cpp
+++ TunnelsOfTerror/Source.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<string>
 using namespace std;
 
 
@@ -17,22 +18,26 @@ public:
 	Location* east = nullptr;
 
 	void setExits(Location*,Location*,Location*,Location*);
-	
+	void getDesc();
 
-	Location(string);
+	Location(string,string);
 	~Location();
 };
 
 //LOCATION FUNCTIONS
-Location::Location(string n)
+Location::Location(string n,string d)
 {
 	name = n;
+	desc = d;
 }
 Location::~Location()
 {
 
 }
-
+void Location::getDesc()
+{
+	cout << desc << endl;
+}
 void Location::setExits(Location* n, Location* s, Location* w, Location* e)
 {
 	north = n;
@@ -53,6 +58,7 @@ public:
 
 	void setLocation(Location*);
 	Location* getLocation();
+	void travel();
 };
 
 //PLAYER FUNCTIONS
@@ -69,6 +75,38 @@ Location* Player::getLocation()
 	return currentLoc;
 }
 
+void Player::travel()
+{
+	int direction = 0;  // 1-north 2-south 3-west 4-east 	
+	bool north = false;
+	bool south = false;
+	bool west = false;
+	bool east = false;
+	if (currentLoc->north != nullptr)
+		north = true;
+	if (currentLoc->south != nullptr)
+		south = true;
+	if (currentLoc->west != nullptr)
+		west = true;
+	if (currentLoc->west != nullptr)
+		west = true;
+	cout << "Where do you want to travel? ";
+
+	while (direction < 1 || direction>4)
+		cin >> direction;
+	if (direction == 1 && north == true)
+	{
+		setLocation(currentLoc->north);
+	}
+	else if (direction == 1 && north == false)
+	{
+		cout << "You cannot travel north! Choose another direction." << endl;
+		currentLoc->getDesc();
+
+	}
+}
+
+
 class World
 {
 public:
@@ -79,19 +117,19 @@ public:
 	World()
 	{
 		//creating locations
-		Location* en = new Location("Entrance");
-		Location* dc = new Location("Damp Cavern");
-		Location* gr = new Location("Guard Room");
-		Location* ba = new Location("Barracks");
-		Location* wr = new Location("Well Room");
-		Location* kt = new Location("Kitchen");
-		Location* ib = new Location("Ice Box");
-		Location* ld = new Location("Larder");
-		Location* gh = new Location("Great Hall");
-		Location* br = new Location("Bottle Room");
-		Location* tr = new Location("Trolls Room");
-		Location* lb = new Location("Library");
-		Location* mr = new Location("Mysterious Room");
+		Location* en = new Location("Entrance","You arrive at the Entrance. You can go north(1) to Damp Cavern.");
+		Location* dc = new Location("Damp Cavern","You find yourself in the Damp Cavern, you can travel north to (1)Well Room, south to (2)Entrance, west to (3)Guard Room and east to the (4)Kitchen.");
+		Location* gr = new Location("Guard Room", "You are in Guard Room, you can travel north to (1)Barracks and east to the (4)Damp Cavern.");
+		Location* ba = new Location("Barracks", "You see many beds and weapons rakcks by the walls. You must be in Barracks, you travel south to (2)Guard Room.");
+		Location* wr = new Location("Well Room","You are in the Well Room, you can go south to (2)Damp Cavern or east to  the (4)Larder.");
+		Location* kt = new Location("Kitchen","You are in the Kitchen. You can travel in every direction. (1)Great Hall, (2)Ice Box, (3)Damp Cavern, (4)Larder.");
+		Location* ib = new Location("Ice Box","You feel cold. You see ice. Ice Box. Cold. Go north to (1)Kitchen.");
+		Location* ld = new Location("Larder","You are in the Larder, you can go north to (1)Bottle Room, south to (2)Well Room or west to (3)Kitchen.");
+		Location* gh = new Location("Great Hall","You are in the Great Hall. It looks trully impressive. You can travel north to (1)Bottle Room, south to (2)Kitchen, west to (3)Trolls Room or east to (4)Mysterious Room.");
+		Location* br = new Location("Bottle Room" ,"You are in Bottle Room, you see passage to the (2)Larder on the south and entrance to the (3)Great Hall to the west.");
+		Location* tr = new Location("Trolls Room","You see trolls gathered around a bonfire, better keep quiet. You can go to the (1)Library or (4)Mysterious Room.");
+		Location* lb = new Location("Library","Books, books, books, so much knowlegde, if only you could read... You see only one door leading south to (2)Trolls Room... but wait! You spot a secret entrence to the (4)Mysterious Room.");
+		Location* mr = new Location("Mysterious Room","You are in the Mysterious Room. You get the whole mystery now. Not that amazing. You can travel north to (1)Library, south to (2)Great Hall");
 		
 		//setting up exits for each location
 		en->setExits(dc, nullptr, nullptr, nullptr);
@@ -132,11 +170,32 @@ public:
 
 int main()
 {
+
+	
+
 	World* world = new World();
 
 	Player* player = new Player();
-	
 
+	player->setLocation(world->locations.front());
+
+	//game loop
+
+	cout << "Welcome to Tunnels of Terror!";
+
+
+	while (player)
+	{
+
+		player->currentLoc->getDesc();
+		
+
+		cout << "Where do you want to go?" << endl;
+
+	}
+	
+	getchar();
+	getchar();
 	
 
 	return 0;
